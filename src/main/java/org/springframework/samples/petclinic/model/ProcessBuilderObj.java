62c9a8072761b1b9fd209967d31d3f72dcaaa983
Sabin Thomas <sabin@codiscope.com>
2016-04-07 23:03:45 -0400

    Adding ProcessBuilderDemo class to see effect on scan

---
+++ src/main/java/org/springframework/samples/petclinic/model/ProcessBuilderObj.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic.model;
+
+public class ProcessBuilderDemo {
+
+	public ProcessBuilderDemo(){
+	   // create a new list of arguments for our process
+	   String[] list = {"notepad.exe", "test.txt"};
+
+	   // create the process builder
+	   ProcessBuilder pb = new ProcessBuilder(list);
+
+	   // set the command list
+	   pb.command(list);
+
+	   // print the new command list
+	   System.out.println("" + pb.command());		
+		
+	}
+}
